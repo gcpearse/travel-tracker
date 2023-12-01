423bc8a4bf4a2f834c2fd1057d7f990ec70e49c8
gcpearse <glen.c.pearse@gmail.com>
2023-12-01 18:34:59 +0000

    minor reformatting

--- travel-tracker/src/components/Locations.jsx
+++ travel-tracker/src/components/Locations.jsx
@@ -1,14 +1,19 @@
 import Location from "./Location";
 
 const Locations = ({ locations, setVisitCount }) => {
-  return <div>
-    <ol id="locations-list">
-      {locations.map((location) => {
-        return <Location key={location.name} location={location} setVisitCount={setVisitCount} />;
-      }
-      )}
-    </ol>
-  </div>;
+  return (
+    <div>
+      <ol id="locations-list">
+        {locations.map((location) => {
+          return <Location
+            key={location.name}
+            location={location}
+            setVisitCount={setVisitCount}
+          />;
+        })}
+      </ol>
+    </div>
+  );
 };
 
 export default Locations;
