ae570f40e815add1ddfdabef348263071bcd5c8b
gcpearse <glen.c.pearse@gmail.com>
2023-12-01 18:14:22 +0000

    reformatting

--- travel-tracker/src/components/Header.jsx
+++ travel-tracker/src/components/Header.jsx
@@ -1,8 +1,10 @@
 const Header = () => {
-  return <div id="hero">
-    <h1 id="app-header">Welcome to <span id="app-name">GoMapp</span></h1>
-    <h2 id="welcome-msg">Your personal travel wishlist</h2>
-  </div>;
+  return (
+    <div id="hero">
+      <h1 id="app-header">Welcome to <span id="app-name">GoMapp</span></h1>
+      <h2 id="welcome-msg">Your personal travel wishlist</h2>
+    </div>
+  );
 };
 
 export default Header;
