bbed4103aea0ca9750247c6567321db14c10ed1e
gcpearse <glen.c.pearse@gmail.com>
2023-12-01 18:35:22 +0000

    refactoring to move functionality outside button element

--- travel-tracker/src/components/Location.jsx
+++ travel-tracker/src/components/Location.jsx
@@ -11,52 +11,62 @@ const Location = ({ location, setVisitCount }) => {
   if (isVisited) visitedButtonStyle = "green-button";
   if (onWishList) wishlistButtonStyle = "green-button";
 
-  return <li className="country">
-    <img className="image" src={location.image} alt="An image representing the country" />
-    <p className="country-fact" id="country-name">{location.name}</p>
-    <button className="list-button" id={visitedButtonStyle} onClick={() => {
-      setIsVisited((bool) => {
-        return !bool;
-      });
-      setOnWishList(() => {
-        return false;
-      });
-      setVisitCount((current) => {
-        if (!isVisited) {
-          if (current < 10) {
-            return current + 1;
-          } else {
-            return current;
-          }
-        } else {
-          if (current > 0) {
-            return current - 1;
-          } else {
-            return current;
-          }
-        }
-      });
-    }}>Visited</button>
-    <button className="list-button" id={wishlistButtonStyle} onClick={() => {
-      setOnWishList((bool) => {
-        return !bool;
-      });
-      setIsVisited(() => {
-        return false;
-      });
-      setVisitCount((current) => {
-        if (!onWishList) {
-          if (current > 0 && isVisited) {
-            return current - 1;
-          } else {
-            return current;
-          }
+  const handleVisitedClick = () => {
+    setIsVisited((bool) => {
+      return !bool;
+    });
+    setOnWishList(() => {
+      return false;
+    });
+    setVisitCount((current) => {
+      if (!isVisited) {
+        if (current < 10) {
+          return current + 1;
         } else {
           return current;
         }
-      });
-    }}>Add to wishlist</button>
-  </li>;
+      } else {
+        if (current > 0) {
+          return current - 1;
+        } else {
+          return current;
+        }
+      }
+    });
+  };
+
+  const handleWishlistClick = () => {
+    setOnWishList((bool) => {
+      return !bool;
+    });
+    setIsVisited(() => {
+      return false;
+    });
+    setVisitCount((current) => {
+      if (!onWishList) {
+        if (current > 0 && isVisited) {
+          return current - 1;
+        } else {
+          return current;
+        }
+      } else {
+        return current;
+      }
+    });
+  };
+
+  return (
+    <li className="country">
+      <img
+        className="image"
+        src={location.image}
+        alt="An image representing the country"
+      />
+      <p className="country-fact" id="country-name">{location.name}</p>
+      <button className="list-button" id={visitedButtonStyle} onClick={handleVisitedClick}>Visited</button>
+      <button className="list-button" id={wishlistButtonStyle} onClick={handleWishlistClick}>Add to wishlist</button>
+    </li>
+  );
 };
 
 export default Location;
