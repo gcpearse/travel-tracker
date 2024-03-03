7105c0f39805c5c3e2d0dbcc2044d645187106b1
gcpearse <glen.c.pearse@gmail.com>
2024-03-03 21:34:15 +0000

    adds event listener to show and hide scroll button

--- travel-tracker/src/components/Footer.jsx
+++ travel-tracker/src/components/Footer.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 const Footer = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 1200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  });
+
   return (
-    <button className="back-to-top-button" onClick={() => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    }}>Back to top</button>
+    <button
+      className={!isVisible ? (
+        "back-to-top-button"
+      ) : (
+        "back-to-top-button visible-button"
+      )}
+      onClick={() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}>Back to top</button>
   );
 };
 
